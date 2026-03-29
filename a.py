8ea80e579865bfa0ff4f76eab8352f757bfe0621
MuhammadMouidAli1 <i221026@nu.edu.pk>
2026-03-30 00:19:31 +0500

    remove a.py

--- a.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyB4pFQKLM1DXA-bqfeLkW3RndY3sUH4kIY")
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
